24eae953c45a63d6a98a741b8e4953dc6c4e5993
leongwenxuan <leongwenxuan0@gmail.com>
2025-03-19 16:09:11 +0800

    changed db and added new profile route

--- fypapp/settings.py
+++ fypapp/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'fypapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fyp_project',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DB_PASSWORD'),  
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
