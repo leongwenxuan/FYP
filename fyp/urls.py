c451f8d9108f195334b70b3dd4f2ccba3ecdba7e
leongwenxuan <leongwenxuan0@gmail.com>
2025-03-21 16:33:05 +0800

    removed files

--- fyp/urls.py
+++
@@ -1,7 +0,0 @@
-from fypapp.views import transcribe_audio
-
-urlpatterns = [
-    # ... existing urls ...
-    path('api/transcribe-audio/', transcribe_audio, name='transcribe-audio'),
-    # ... existing urls ...
-] 
